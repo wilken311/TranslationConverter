cd885ca41cb5c6fd70e94e997f71c3a6ad43f304
wilkenmontero <wilken.montero@zzagl.me>
2021-04-30 19:25:50 +0800

    Updated Files

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-/Translation1
-1. Export data from notepad to JSON Format
+Instruction:
 
-/Translation2
-1. Export data from excel to JSON Format
+Translation1 Folder- Export data from notepad to JSON Format 
+  1. Export data from notepad to JSON Format
+
+Translation2 Folder - Export data from excel to JSON Format
+  1. Run "npm install"
+  2. Navigate to /Translation2
+  3. Insert your excel file (e.g sample.xlsx)
+  4. Replace the following in TranslationConverter2.js
+
+     //Replace the "sample.xlsx" with the name of your excel file
+     Line 6:  source: fs.readFileSync('sample.xlsx'), 
+  
+     //Replace the "Sheet1" with the name of the sheet you want to convert.  
+     Line 7:  sheets: ['Sheet1']
+     Line 10: let sheet= result.Sheet1;
+
+  5. Run "node TranslationConverted2.js"
+  6. Result will be seen on ar.json and en.json file.
+
+Note: Please follow the format on "sample.xlsx"
+Thank You!
+
+
+
--- Translation2/TranslationConverter2.js
+++ Translation2/TranslationConverter2.js
@@ -3,21 +3,12 @@ const excelToJson = require('convert-excel-to-json');
 const fs = require('fs');
  
 const result = excelToJson({
-    source: fs.readFileSync('english-arabic.xlsx'), // fs.readFileSync return a Buffer
-    sheets: ['partner_mobile_app'],
-    // sheets: ['Sheet1'],
-    columnToKey: {
-        A: 'english',
-        B: 'arabic'
-    },
-    header:{
-        // Is the number of rows that will be skipped and will not be present at our result object. Counting from top to bottom
-        rows: 1 // 2, 3, 4, etc.
-    }
+    source: fs.readFileSync('sample.xlsx'), 
+    sheets: ['Sheet1'],
 });
 
-let sheet= result.partner_mobile_app;
-// let sheet= result.Sheet1;
+let sheet= result.Sheet1;
+
 
 Object.prototype.push = function( key, value ){
     this[key] = value;
@@ -30,25 +21,22 @@ var arObj = {};
 for(var i = 0, iMax = sheet.length; i < iMax; i++){
     
     //Remove special characters
-    let keyFilter1=sheet[i].english.replace(/[^a-zA-Z ]/g, "");
+    let keyFilter1=sheet[i].A.replace(/[^a-zA-Z ]/g, "");
 
     //Replace all white space into underscore and make it all lowercase
     let keyFilter2=keyFilter1.replace(/\s+/g, '_').toLowerCase();
     
     let key=keyFilter2;
    
-    let english=sheet[i].english;
-    let arabic=sheet[i].arabic;
+    let english=sheet[i].A;
+    let arabic=sheet[i].B;
 
     enObj.push(key, english);   
     arObj.push(key, arabic); 
 
 }
 
-// console.log(enObj)
 fs.writeFileSync('en.json', JSON.stringify(enObj));
-
-// console.log(arObj)
 fs.writeFileSync('ar.json', JSON.stringify(arObj));
 
 console.log("Converted Successfully")
